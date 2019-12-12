61409da55e8deee1b478569afb5f627ea68c6aa5
John Bonde <jjbonde1@gmail.com>
2019-12-12 11:38:09 -0700

    fixed errors

--- server/main.js
+++ server/main.js
@@ -50,7 +50,7 @@ server.use("/api/moons", new MoonsController().router);
 
 //NOTE Default error handler, catches all routes with an error attached
 server.use((error, req, res, next) => {
-  res.status(error.status).send({ error: { message: error.message } });
+  res.status(error.status || 400).send({ error: { message: error.message } });
 });
 
 //NOTE Catch all to insure to return 404 if recieved a bad route
